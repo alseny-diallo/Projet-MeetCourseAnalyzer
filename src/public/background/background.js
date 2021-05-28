362f29e160d437f128cefb565210f4d0aeec4f9a
Idrissa SOW <idrissasow@esp.sn>
2021-05-28 15:23:41 +0000

    Modification du fichier background.js

--- src/public/background/background.js
+++ src/public/background/background.js
@@ -2,11 +2,33 @@ console.log('background running');
 chrome.runtime.onInstalled.addListener(function(){
     alert("Merci d'avoir  mis à jour l'extension");
     });
-
- chrome.webNavigation.onCompleted.addListener(function(){
+   
+//window.onbeforeunload
+/*    chrome.runtime.onbeforeunload = function() {
+        // tes actions
+        alert("Votre fenêtre cést fermé");
+    };
+*/
+ /*chrome.webNavigation.onCompleted.addListener(function(){
     alert("Welcome to the Google Meet! ");
     },
     {
         url: [{urlMatches:"meet.google.com"}]  
     });
+    */
+/*
+chrome.tabs.onActivated.addListener(
+    tab=>{
+        chrome.tabs.get(tab.tabId, current_tab_info =>{
+            if(/^https:\/\/meet\.google/.test(current_tab_info.url)){
+                chrome.tabs.executeScript(null, {file:'src/public/js/content.js'}, ()=>console.log(i, injection));
+            }
+        });
+    });
 
+*/
+/*let url;
+chrome.tabs.query(queryInfo, function (tabs) {
+            url = tabs[0].url;
+        });
+*/
